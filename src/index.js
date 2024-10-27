1fd7d0fe0f47ea61eca487324250b20d1758286a
Your Name <you@example.com>
2024-10-27 18:10:26 +0100

    signals

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import Database from './database.js';
 import DataGeneric from './data-generic.js';
 import { Log } from './log.js';
+import { Signals } from './signals';
 
 export { Database, DataGeneric, Log } ;
 
--- src/signals.ts
+++ src/signals.ts
@@ -2,49 +2,70 @@ import { Log } from './log';
 
 class Signals {
 
- constructor(allowedEvents) {
+ constructor(allowedEvents, clients, send: (clientData: any, msg: any) => void) {
   this.allowedEvents = allowedEvents;
-  this.clients = new Map();
+  this.clients = clients;
+  this.send = send;
  }
+
+
+ clientDataByWsGuid(wsGuid) {
+  let r = this.clients.get(wsGuid);
+  if (!r)
+   return null;
+  if (!r.subscriptions)
+   r.subscriptions = new Set();
+ }
+
 
  subscribe(c) {
   if (!c.params) return { error: 1, message: 'Parameters are missing' };
   if (!c.params.event) return { error: 2, message: 'Event parameter is missing' };
   if (!this.allowedEvents.includes(c.params.event)) return { error: 3, message: 'Unsupported event name' };
-  const clientData = this.clients.get(c.wsGuid);
+  const clientData = this.clientDataByWsGuid(c.wsGuid);
   if (!clientData) return { error: 4, message: 'Client not found' };
-  clientData.userID = c.userID;
   clientData.subscriptions.add(c.params.event);
   Log.debug('Client ' + c.ws.remoteAddress + ' subscribed to event: ' + c.params.event);
   return { error: 0, message: 'Event subscribed' };
  }
 
- notifySubscriber(userID, event, data) {
+
+ notify(event, data) {
   for (const [wsGuid, clientData] of this.clients) {
-   if (clientData.userID === userID && clientData.subscriptions.has(event)) {
+   if (clientData.subscriptions?.has(event)) {
     const msg = { event, data });
     Log.debug('Send event to: ' + wsGuid + ', message: ' + msg);
-    this.send(clientData, msg);
+    this.send(wsGuid, clientData, msg);
+   }
+  }
+  return { error: 0, message: 'Event sent' };
+ }
+
+
+ notifyUser(userID, event, data) {
+  for (const [wsGuid, clientData] of this.clients) {
+   if (clientData.userID === userID && clientData.subscriptions?.has(event)) {
+    const msg = { event, data });
+    Log.debug('Send event to: ' + wsGuid + ', message: ' + msg);
+    this.send(wsGuid, clientData, msg);
    }
   }
  }
 
- userUnsubscribe(c) {
+
+ unsubscribe(c) {
   if (!c.params) return { error: 1, message: 'Parameters are missing' };
   if (!c.params.event) return { error: 2, message: 'Event parameter is missing' };
   if (!this.allowedEvents.includes(c.params.event)) return { error: 3, message: 'Unsupported event name' };
-  const clientData = this.webServer.wsClients.get(c.ws);
+  const clientData = this.clientDataByWsGuid(c.wsGuid);
   if (!clientData) return { error: 4, message: 'Client not found' };
-  if (!clientData.subscriptions?.has(c.params.event))
+  if (!clientData.subscriptions.has(c.params.event))
    return {
     error: 5,
     message: 'Client is not subscribed to this event',
    };
-  clientData.subscriptions?.delete(c.params.event);
+  clientData.subscriptions.delete(c.params.event);
   return { error: 0, message: 'Event unsubscribed' };
  }
-
- removeClient(wsGuid) {
-  this.clients.delete(wsGuid);
- }
+}
 
