1e9872f4bd6c46d276a837b87e3f663f4d11768a
Your Name <koom@c1>
2025-01-29 18:26:23 +0100

    fix module logging config

--- src/module-app-base.ts
+++ src/module-app-base.ts
@@ -93,7 +93,7 @@ export class ModuleAppBase {
     Log.debug('WebSocket message from: ', ws.remoteAddress, ', message: ', message);
     const res = await api.processWsMessage(ws, message);
     if (res) {
-     Log.info('WebSocket response to: ', ws.remoteAddress, ', message: ', res);
+     Log.debug('WebSocket response to: ', ws.remoteAddress, ', message: ', res);
      ws.send(JSON.stringify(res));
     }
    },
